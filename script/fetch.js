c27244426cf4ca777159e1d450a1a057d8da262e
QueAnka <annika.nylin@hotmail.com>
2023-02-25 15:05:05 +0100

    strukturerat om koden i rätt filer

--- script/fetch.js
+++ script/fetch.js
@@ -1,15 +1,5 @@
-// DOM element//
-
-const searchInput = document.querySelector('#search-input')
-const preViewPicture = document.querySelector('#preview-picture')
-const pokemonsPreviewContainer = document.querySelector('#preview-picture')
-const SearchButton = document.querySelector('#search-button')
-
-
-// Här sparas all data:
-
-let pokemonList = []
-console.log('Detta sparas i variabeln pokemonList', pokemonList);
+export {renderUI}
+export {pokemonList}
 
 // för att hämta api
 
@@ -24,7 +14,6 @@ async function getApi(url){
 	
 }
 
-
 async function renderUI(){
 	const pokemons = await getApi(pokeUrl)
 	
@@ -34,73 +23,26 @@ console.log('detta hämtas från API', pokemons);
 		const response = await fetch(pokemon.url)
 		const data = await response.json()
 		
-		// const imgUrl = data.sprites.front_default
 		
-		const pokemonInfo = {
+		const pokemon = {
 		name: data.name,
 		picture: data.sprites.front_default,
 		}
 		
 
-		pokemonList.push(pokemonInfo)
+		pokemonList.push(pokemon)
 
 	
 	});
 
 }
 
-// Kallar på funktionen för att hämta data från API och lägga i variabel
-
-  renderUI()
-
-
-
-//   console.log(renderUI());
+// Här sparas all hämtad data:
+let pokemonList = []
+console.log('Detta sparas i variabeln pokemonList', pokemonList);
 
 
-// vad vill jag att sökfunktionen ska göra?
-	// loopa igenom variabeln pokemonCard
-	// Jämföra söksträng med name, med filter
-	// skapa en variabel med alla namn som  matchar
-	// 
 
-// Söka på valfritt ord och matcha mot sparade data
-
-searchInput.addEventListener('keyup', async (event) =>{ 
-	 let searchString = event.target.value
-	console.log('Key up: ', searchString)
-	
-	
-		let matches = pokemonList.filter(list => list.name.includes(searchString))
-	console.log(matches);
-
-	pokemonsPreviewContainer.innerHTML=''
-
-
-	matches.forEach(pokemonInfo =>{
-	let pokemonCard = document.createElement('article')
-	let pokemonCardImage = document.createElement('div')
-	 pokemonCardImage.innerHTML = `<img src = "${pokemonInfo.picture}" alt = "${pokemonInfo.name}">`
-	let pokemonCardName = document.createElement('p')
-	pokemonCardName.innerText = pokemonInfo.name
-	let choseButton = document.createElement('button')
-	choseButton.innerText ='Lägg till'
-
-	pokemonCard.classList ='pokemon-card'
-	pokemonCardImage.classList = 'pokemon-image'
-	pokemonCardName.classList = 'pokemon-card-name'
-
-
-	pokemonCard.append(pokemonCardImage)
-	pokemonCard.append(pokemonCardName)
-	pokemonCard.append(choseButton)
-
-
-	pokemonsPreviewContainer.append(pokemonCard)
-
-	})
-
-})
 
 
 
--- script/script.js
+++ script/script.js
@@ -1,8 +1,17 @@
+import { renderUI } from "./fetch.js"
+import { pokemonList } from "./fetch.js"
+
 // DOM-element variabler //
 const searchBtn = document.querySelector('#search-view-btn')
 const viewTeamBtn = document.querySelector('#team-view-btn')
 const searchViewSection = document.querySelector('.search-view_section')
 const teamViewSection = document.querySelector('.team-view_section')
+const searchInput = document.querySelector('#search-input')
+const pokemonsPreviewContainer = document.querySelector('#preview-picture')
+
+
+
+
 
 
 
@@ -18,3 +27,62 @@ viewTeamBtn.addEventListener
 	teamViewSection.style.display = 'block'
 })
 
+// Kallar på funktionen för att hämta data från API och lägga i variabel
+
+renderUI()
+
+
+
+//   console.log(renderUI());
+
+
+// vad vill jag att sökfunktionen ska göra?
+	// loopa igenom variabeln pokemonCard
+	// Jämföra söksträng med name, med filter
+	// skapa en variabel med alla namn som  matchar
+	// 
+
+// Söka på valfritt ord och matcha mot sparade data
+
+searchInput.addEventListener('keyup', async (event) =>{ 
+	 let searchString = event.target.values.toLowerCase()
+	console.log('Key up: ', searchString)
+	
+	
+		let matches = pokemonList.filter(list => list.name.includes(searchString))
+	console.log(matches);
+
+	// töm containern för varje ny matchning
+
+	pokemonsPreviewContainer.innerHTML=''
+
+// visa bild/info för matchande pokemons
+	matches.forEach(pokemon =>{
+	let pokemonCard = document.createElement('article')
+	let pokemonCardImage = document.createElement('div')
+	 pokemonCardImage.innerHTML = `<img src = "${pokemon.picture}" alt = "${pokemon.name}">`
+	let pokemonCardName = document.createElement('p')
+	pokemonCardName.innerText = pokemon.name
+	let pokemonCardButton = document.createElement('button')
+	pokemonCardButton.innerText ='Lägg till'
+
+	pokemonCard.classList ='pokemon-card'
+	pokemonCardImage.classList = 'pokemon-image'
+	pokemonCardName.classList = 'pokemon-card-name'
+	pokemonCardButton.classList = 'pokemon-card-button'
+
+	pokemonCard.append(pokemonCardImage)
+	pokemonCard.append(pokemonCardName)
+	pokemonCard.append(pokemonCardButton)
+
+
+	pokemonsPreviewContainer.append(pokemonCard)
+
+	})
+
+})
+
+// lägg till pokemon i mitt lag
+pokemonCardButton.addEventListener('click', ){
+
+}
